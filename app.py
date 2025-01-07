c5ed6a46ddc340dd50bff2f15864ec29704fd7ed
RahulVachhani <rahul.vachhani@openxcell.com>
2025-01-07 17:19:34 +0530

    add all files

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+
+filename = 'spam_mail_model.sav'
+load_model = pickle.load(open(filename, 'rb'))
+
+feature_extraction = load_model['feature_extraction']
+model = load_model['model']
+
+def diabetes_prediction(input_data):
+    s = feature_extraction.transform(input_data)
+    prediction = model.predict(s)
+    if (prediction[0] == 0):
+      return 'The mail is spam mail'
+    else:
+      return 'The mail is not spam mail'
+    
+
+
+def main():
+   
+    st.title('Mail Prediction Web APP')
+
+
+    message = st.text_area('Enter your mail')
+
+
+    diagnosis = ''
+
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([message])
+
+    st.success(diagnosis)
+
+
+if __name__ == '__main__':
+    main()
